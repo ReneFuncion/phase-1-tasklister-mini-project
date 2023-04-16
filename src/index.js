0743f5324b666d6de1cea4cd9b8ca9e125b1c5c9
Rene Funcion <rcfun959@gmail.com>
2023-04-15 23:49:27 -0700

    Completed phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  //console.log(form);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDoList(e.target.querySelector("#new-task-description").value);
+    form.reset();
+  });
+
 });
+
+function buildToDoList(todo) {
+  let liItem = document.createElement("li");
+  let deleteButton = document.createElement("button");
+  deleteButton.addEventListener("click", handleDelete);
+  deleteButton.textContent = "X";
+  liItem.textContent = `${todo}  `;
+  liItem.appendChild(deleteButton);
+  console.log(liItem);
+  document.querySelector("#tasks").appendChild(liItem);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
